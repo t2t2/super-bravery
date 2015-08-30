7fe779df9417c8f9090d6531595a3788dbfb394b
t2t2 <taavot@gmail.com>
2015-08-30 20:24:15 +0300

    Added 5 minute cache for frontend config to help any future reddit hugs

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,7 +2,7 @@
 
 namespace t2t2\SuperBravery\Http\Controllers;
 
-
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
 use t2t2\SuperBravery\MetaState;
@@ -26,13 +26,18 @@ class FrontendController extends Controller {
 	 *
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-	public function config(Manager $manager, MetaState $metaState) {
+	public function config(CacheRepository $cache) {
 
-		$manager->parseIncludes(['champions', 'items', 'maps', 'summoner_spells']);
+		$response = $cache->remember('frontend.config', 5, function () {
+			$manager = app(Manager::class);
+			$metaState = app(MetaState::class);
 
-		$state = new Item($metaState, new MetaTransformer(), 'state');
+			$manager->parseIncludes(['champions', 'items', 'maps', 'summoner_spells']);
 
-		$response = $manager->createData($state)->toArray();
+			$state = new Item($metaState, new MetaTransformer(), 'state');
+
+			return $manager->createData($state)->toArray();
+		});
 
 		return response()->json($response);
 	}
